35c71841bde6d479ca41a82a79cfd3c56968ac7b
Andrej Hristov <andrejhris23@gmail.com>
2021-03-11 22:35:15 +0100

    testing phase 2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        connection: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: false
     }
 });
